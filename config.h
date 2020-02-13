0fbb06fde1605c36c7e59971ada60eac41f0297c
Sepehr Hamzehlouy <s.hamzelooy@gmail.com>
2020-02-14 01:44:43 +0330

    Better way to add libraries

--- config.h
+++ config.h
@@ -1,6 +1,33 @@
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
+/* Define to 1 if you have the <inttypes.h> header file. */
+/* #undef HAVE_INTTYPES_H */
+
+/* Define to 1 if you have the <memory.h> header file. */
+/* #undef HAVE_MEMORY_H */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+/* #undef HAVE_STDINT_H */
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+/* #undef HAVE_STDLIB_H */
+
+/* Define to 1 if you have the <strings.h> header file. */
+/* #undef HAVE_STRINGS_H */
+
+/* Define to 1 if you have the <string.h> header file. */
+/* #undef HAVE_STRING_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+/* #undef HAVE_SYS_STAT_H */
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+/* #undef HAVE_SYS_TYPES_H */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+/* #undef HAVE_UNISTD_H */
+
 /* Name of package */
 #define PACKAGE "learn2libuv"
 
@@ -22,5 +49,12 @@
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "0.1"
 
+/* Define to 1 if you have the ANSI C header files. */
+/* #undef STDC_HEADERS */
+
 /* Version number of package */
 #define VERSION "0.1"
+
+/* Define to the type of a signed integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int64_t */
